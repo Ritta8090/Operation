d7a74b06f2f7432ff47eaedeba1a2192304acb9e
Ritta8090 <Ritta8090@gmail.com>
2023-04-11 00:06:06 +0800

    貸款計算金額

--- Operation/2_Loan.Designer.cs
+++ Operation/2_Loan.Designer.cs
@@ -45,9 +45,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(67, 61);
+            this.label1.Location = new System.Drawing.Point(89, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.Size = new System.Drawing.Size(94, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "貸款金額 ";
             // 
@@ -55,9 +56,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(67, 101);
+            this.label2.Location = new System.Drawing.Point(89, 126);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.Size = new System.Drawing.Size(81, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "期限(年)";
             // 
@@ -65,9 +67,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(67, 144);
+            this.label3.Location = new System.Drawing.Point(89, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.Size = new System.Drawing.Size(77, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "利率(%)";
             // 
@@ -75,72 +78,83 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.Location = new System.Drawing.Point(67, 185);
+            this.label4.Location = new System.Drawing.Point(89, 231);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.Size = new System.Drawing.Size(69, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "頭期款";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(172, 55);
+            this.textBox1.Location = new System.Drawing.Point(229, 69);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(132, 25);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(172, 95);
+            this.textBox2.Location = new System.Drawing.Point(229, 119);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(132, 25);
             this.textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(172, 138);
+            this.textBox3.Location = new System.Drawing.Point(229, 172);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(132, 25);
             this.textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(172, 179);
+            this.textBox4.Location = new System.Drawing.Point(229, 224);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.Size = new System.Drawing.Size(132, 25);
             this.textBox4.TabIndex = 7;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(41, 259);
+            this.button1.Location = new System.Drawing.Point(55, 324);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 36);
+            this.button1.Size = new System.Drawing.Size(117, 45);
             this.button1.TabIndex = 8;
             this.button1.Text = "PMT(月付)";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(172, 259);
+            this.button2.Location = new System.Drawing.Point(229, 324);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 36);
+            this.button2.Size = new System.Drawing.Size(117, 45);
             this.button2.TabIndex = 9;
             this.button2.Text = "總付款";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(309, 259);
+            this.button3.Location = new System.Drawing.Point(412, 324);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(88, 36);
+            this.button3.Size = new System.Drawing.Size(117, 45);
             this.button3.TabIndex = 10;
             this.button3.Text = "Report";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // callForm3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(505, 402);
+            this.ClientSize = new System.Drawing.Size(673, 502);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -152,6 +166,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "callForm3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "貸款金額計算";
--- Operation/2_Loan.cs
+++ Operation/2_Loan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Operation
 {
@@ -16,5 +17,45 @@ namespace Operation
         {
             InitializeComponent();
         }
+
+        
+        
+        
+        
+        
+        
+        //頭期款
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tatle = int.Parse(textBox1.Text);  //貸款金額
+            int month = int.Parse(textBox2.Text)*12;  //期限(年->月)
+            double rate = double.Parse(textBox3.Text)/100/12;  //利率(%)
+            int payment = int.Parse(textBox4.Text);  //頭期款
+
+            string resoult = "";
+            double calculate = (Math.Pow((1 + rate), month) * rate)/ (Math.Pow((1 + rate), month) - 1);
+            int calculate2 = (int)(tatle * calculate);
+            resoult = Convert.ToString(calculate2);
+            MessageBox.Show("月付額: "+resoult + "元");
+
+            //每月應付本息金額之平均攤還率＝{[(1＋月利率)^月數]×月利率}÷{[(1＋月利率)^月數]－1}
+            //平均每月應攤付本息金額＝貸款本金×每月應付本息金額之平均攤還率＝每月應還本金金額＋每月應付利息金額
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
+
+
+   
+    
 }
