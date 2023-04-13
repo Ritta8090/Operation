5a58dc9eb1a44eb47d7fe209346712727d79b977
Ritta Huang <ritta302@yahoo.com.tw>
2023-04-13 22:13:45 +0800

    測試取得最新

--- Operation/Program.cs
+++ Operation/Program.cs
@@ -14,6 +14,7 @@ namespace Operation
         [STAThread]
         static void Main()
         {
+            string a = "123";
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form0());
