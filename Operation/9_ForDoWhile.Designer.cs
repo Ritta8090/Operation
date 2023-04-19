d202e21c5f3f8c414142161ee444d4eed3861b1d
Ritta Huang <ritta302@yahoo.com.tw>
2023-04-19 18:11:34 +0800

    0419

--- Operation/9_ForDoWhile.Designer.cs
+++ Operation/9_ForDoWhile.Designer.cs
@@ -34,15 +34,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.labresult = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(37, 32);
+            this.label1.Location = new System.Drawing.Point(28, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.Size = new System.Drawing.Size(61, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Number: ";
             // 
@@ -50,24 +53,27 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(40, 369);
+            this.label2.Location = new System.Drawing.Point(30, 295);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 23);
+            this.label2.Size = new System.Drawing.Size(67, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Result:";
             // 
             // txtnumber
             // 
-            this.txtnumber.Location = new System.Drawing.Point(108, 24);
+            this.txtnumber.Location = new System.Drawing.Point(81, 19);
+            this.txtnumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnumber.Name = "txtnumber";
-            this.txtnumber.Size = new System.Drawing.Size(80, 25);
+            this.txtnumber.Size = new System.Drawing.Size(61, 22);
             this.txtnumber.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 63);
+            this.button1.Location = new System.Drawing.Point(19, 50);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 33);
+            this.button1.Size = new System.Drawing.Size(135, 26);
             this.button1.TabIndex = 3;
             this.button1.Text = "輸入的數為奇數或偶數?";
             this.button1.UseVisualStyleBackColor = true;
@@ -78,27 +84,53 @@
             this.labresult.AutoSize = true;
             this.labresult.BackColor = System.Drawing.SystemColors.Info;
             this.labresult.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.labresult.Location = new System.Drawing.Point(44, 405);
+            this.labresult.Location = new System.Drawing.Point(33, 324);
+            this.labresult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labresult.Name = "labresult";
-            this.labresult.Size = new System.Drawing.Size(49, 20);
+            this.labresult.Size = new System.Drawing.Size(39, 16);
             this.labresult.TabIndex = 4;
             this.labresult.Text = "結果";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(25, 139);
+            this.button2.Location = new System.Drawing.Point(19, 92);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(180, 33);
+            this.button2.Size = new System.Drawing.Size(135, 26);
             this.button2.TabIndex = 5;
             this.button2.Text = "陣列arr0711[]最大/小值";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(177, 15);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 26);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "陣列arr0711[]統計奇偶數的量";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(177, 50);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(172, 26);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "陣列arr0711_Str[]最長的名字";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // callForm9
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 495);
+            this.ClientSize = new System.Drawing.Size(507, 396);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.labresult);
             this.Controls.Add(this.button1);
@@ -106,7 +138,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "callForm9";
             this.Text = "My Homework_0711_Method";
             this.ResumeLayout(false);
@@ -122,5 +153,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label labresult;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Operation/9_ForDoWhile.cs
+++ Operation/9_ForDoWhile.cs
@@ -19,22 +19,17 @@ namespace Operation
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             int i;
             bool number = int.TryParse(txtnumber.Text, out i);
 
             if (number == true)
             {
-                if (i % 2 > 0)
-                {
-                    labresult.Text = $"輸入的數{txtnumber.Text}為奇數";
-                }
-                else
-                {
-                    labresult.Text = $"輸入的數{txtnumber.Text}為偶數";
-                }
+                labresult.Text = i % 2 == 0? $"輸入的數{txtnumber.Text}為偶數": $"輸入的數{txtnumber.Text}為奇數";
+                //if (i % 2 ==0)
+                //    labresult.Text = $"輸入的數{txtnumber.Text}為奇數";
+                //else
+                //    labresult.Text = $"輸入的數{txtnumber.Text}為偶數";
             }
-
             else 
             {
                 MessageBox.Show("請輸入整數");
@@ -49,20 +44,53 @@ namespace Operation
             string showArray="";
             for (int i = 0; i < number.Length; i++)
             {
-
                 showArray += (i == number.Length - 1) ? showArray += number[i]: showArray += number[i] + " , ";
                 //if (i == number.Length - 1)
-                //{
                 //    showArray += number[i];
-                //}
                 //else 
-                //{
                 //    showArray += number[i] + " , ";
-                //}
-                
-                
+                          
             }
             labresult.Text =$"int陣列arr0711 [ {showArray} ] \r\n最大值為{number.Max()}\r\n最小值為{number.Min()}";
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] number = { 1, 5, 6, 8, 7, 97, 54, 887, 65, 878 };
+            string showArray = "";
+            int odd=0, even =0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                showArray += (i == number.Length - 1) ? showArray += number[i] : showArray += number[i] + ",";
+            }
+            for (int i = 0; i % 2 == 0; i++)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+            labresult.Text = $"int陣列arr0711 [ {showArray} ] \r\n奇數共 {odd}\r\n偶數共 {even}";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+
+        string[] myString = { "mother張", "emma", "迪克蕭", "J40", "Candy", "Cindy", "Coconut", "Motherfacker" };
+
+
+
+            int myStringCount = 0;
+            for (int i = 0; i < myString.Length; i++)
+             
+        
+           
+
+            labresult.Text = $"陣列arr0711_Str [ {} ] \r\n最長的名字為 {}";
+        }
+        
+        
     }
 }
