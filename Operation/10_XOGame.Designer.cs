6530a6460c6058b62adc48b9201804f0a736f7d5
Ritta8090 <Ritta8090@gmail.com>
2023-04-25 00:01:02 +0800

    新增 OX Game

--- Operation/10_XOGame.Designer.cs
+++ Operation/10_XOGame.Designer.cs
@@ -28,19 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(231, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X O GAME";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(168, 118);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 75);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(249, 118);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 75);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(330, 118);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 75);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(168, 199);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 75);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(249, 199);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 75);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(330, 199);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 75);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(168, 280);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 75);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(249, 280);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 75);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(330, 280);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 75);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.ClickHandler);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnReset.Location = new System.Drawing.Point(168, 378);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 40);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset (R)";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnExit.Location = new System.Drawing.Point(305, 378);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 40);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit (Esc)";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // callForm10
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 382);
+            this.ClientSize = new System.Drawing.Size(580, 478);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "callForm10";
             this.Text = "X O Game";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
     }
 }
--- Operation/10_XOGame.cs
+++ Operation/10_XOGame.cs
@@ -12,9 +12,124 @@ namespace Operation
 {
     public partial class callForm10 : Form
     {
+        private Button[] xoArray = new Button[9];
+        bool isGameOver = false;     //有連線成功則結束
+        bool isO = false;     //點選是 O 還是 X
+
+        static private int[,] win = new int[,] // int[8,3]
+        {
+            {0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}
+        };
+
         public callForm10()
         {
             InitializeComponent();
+            xoArray = new Button[9] { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+        }
+
+        /// <summary>
+        /// 設定一開始9個按鈕的預設值
+        /// </summary>
+        private void XOButtons()
+        {
+            isGameOver = false;
+            isO = true;
+            for (int i = 0; i < xoArray.Length; i++)
+            {
+                xoArray[i].Text = "";
+                xoArray[i].Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 9顆按鈕的點擊事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClickHandler(object sender, EventArgs e)
+        {
+            Button tmpButton = (Button)sender;
+            tmpButton.Text = isO ? "O" : "X";
+            tmpButton.Enabled = false;
+
+            bool[] result = CheckWinGroup(xoArray);
+            if (result[0] && result[1])
+            {
+
+            }
+            else
+            {
+                isO = !isO;
+            }
+        }
+
+        /// <summary>
+        /// 判斷勝利
+        /// </summary>
+        /// <param name="myControls"></param>
+        /// <returns></returns>
+        private bool[] CheckWinGroup(Button[] myControls)
+        {
+            //gameWinOver {是否有人獲勝, 是否遊戲結束(或是平局)}
+            bool[] gameWinOver = new bool[2] { false, false };
+            int btnIsUse = 1;
+            for (int i = 0; i < 8; i++)
+            {
+                int a = win[i, 0];
+                int b = win[i, 1];
+                int c = win[i, 2];
+                Button b1 = myControls[a];
+                Button b2 = myControls[b];
+                Button b3 = myControls[c];
+
+                //沒有連線就繼續
+                if (b1.Text == "" || b2.Text == "" || b3.Text == "")
+                    continue;
+
+                //連線成功，遊戲結束
+                if (b1.Text == b2.Text && b2.Text == b3.Text)
+                {
+                    MessageBox.Show($"{(isO ? "O" : "X")}選手獲勝", "完局", MessageBoxButtons.OKCancel);
+                    gameWinOver = new bool[2] { true, true };
+                    break;
+                }
+
+                //九格填完，顯示結束
+                if (myControls[i].Text != "")
+                {
+                    btnIsUse++;
+                    if (btnIsUse == 9)
+                    {
+                        gameWinOver[1] = true;
+                        MessageBox.Show($"平手", "完局", MessageBoxButtons.OKCancel);
+                    }
+                }
+            }
+
+            return gameWinOver;
+        }
+
+        /// <summary>
+        /// Reset(R) 重置/重來
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            XOButtons();
+        }
+
+        /// <summary>
+        /// Exist(Esc) 關閉
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
+
